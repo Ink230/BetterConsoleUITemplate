b26647f7547474f184ca277ec95c13d4dcd54654
Ink <inkofdeath230@gmail.com>
2022-02-12 01:44:33 -0500

    Initial Commit

---
+++ BetterConsoleUI/Application.cs
@@ -0,0 +1,21 @@
+﻿namespace App;
+public class Application
+{
+  private readonly IConfiguration _config;
+  private readonly ILogger _logger;
+
+  public Application(IConfiguration config, ILogger logger)
+  {
+    _config = config;
+    _logger = logger;
+  }
+  public async Task Run()
+  {
+    //Do something, ex:
+    Console.WriteLine(Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT"));
+    _logger.Information(_config.GetValue<string>("AppName"));
+    _logger.Information("Application running...");
+    await Task.FromResult(0);
+    Console.ReadLine();
+  }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# BetterConsoleUITemplate
+﻿1. Figure out appsettings.json and appsettings.env.json versions
+2. Set env var on host machine
+3. Setup secrets.json
+4. Configure publish options
+5. Configure Serilog formatting
